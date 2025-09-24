a32e7b4957d85d40166a81e66fe1f96fdb75b4e4
Jooo <76932074+yohanesnaibaho303@users.noreply.github.com>
2025-09-24 14:11:56 +0700

    setup app.tsx dev and federated mode

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,36 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 
-function App() {
-  const [count, setCount] = useState(0)
+const AppPageTest = () => <div>AppTest remotes</div>;
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+/** Used in Host (Without BrowserRouter) */
+const FederatedInner = () => (
+  <Routes>
+    <Route index element={<Navigate to="test" replace />} />
+    <Route path="test" element={<AppPageTest />} />
+    {/* fallback wildcard */}
+    <Route path="*" element={<Navigate to=".." relative="route" replace />} />
+  </Routes>
+);
 
-export default App
+/** Dev standalone (With BrowserRouter) */
+const DevInner = () => (
+  <Routes>
+    <Route path="/" element={<Navigate to="/test" replace />} />
+    <Route path="/test" element={<AppPageTest />} />
+    <Route path="*" element={<Navigate to="/test" replace />} />
+  </Routes>
+);
+
+const App = () => {
+  if (import.meta.env.DEV) {
+    return (
+      <BrowserRouter>
+        <DevInner />
+      </BrowserRouter>
+    );
+  }
+  // federated mode: Host provided BrowserRouter
+  return <FederatedInner />;
+};
+
+export default App;
--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,27 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+import federation from "@originjs/vite-plugin-federation";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), tailwindcss(),
+    federation({
+      name: "remoteTest",
+      filename: "remoteEntry.js",
+      exposes: {
+        "./App": "./src/App.tsx",
+      },
+      shared: ["react", "react-dom", "react-router-dom"],
+    })
+  ],
+  server: {
+    port: 5173,
+  },
+  build: {
+    modulePreload: false,
+    target: "esnext",
+    minify: false,
+    cssCodeSplit: false,
+  },
 })
